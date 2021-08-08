a554179283441c03123cfa4c1bce09c219a4320a
jnerydesigner <jander.webmaster@gmail.com>
2021-08-08 10:48:08 -0400

    feature: population with pagination api

--- frontend/static/js/generals.js
+++ frontend/static/js/generals.js
@@ -1,8 +1,10 @@
 import md5 from './md5.js';
 
+document.addEventListener('DOMContentLoaded', function() {
+  const openModal = document.getElementById('#modal_toggle');
+}, false);
 
-
-
+// Ponto inicial de configuração da API da marvel
 const BASE_URL_INITIAL = 'http://gateway.marvel.com/v1/public/characters?';
 const API_KEY_PUBLIC = "d3d4c96332c02914ba460ab51662f742";
 const API_KEY_PRIVATE = "6c83ae9bd2fe030d9e89c6047a55074b6abd9360";
@@ -10,111 +12,149 @@ const TIME = Date.now();
 const API_PRIVATE_HASH = md5(TIME+API_KEY_PRIVATE+API_KEY_PUBLIC);
 
 
-const modal = document.querySelector('.box-modal-details');
-const openModal = document.querySelector('#modal_toggle');
-const closeModal = document.querySelector('#close-modal');
+// Abre Modal de Herois
 
-openModal.onclick = function(){
-  if(modal.classList.contains('desactive')){
-    modal.classList.remove('desactive');
-    modal.classList.add('active');
-  }
-}
+// const modal = document.querySelector('.box-modal-details');
+// document.getElementById('#modal_toggle').click(() => {
+//   alert('toggle')
+// });
 
-closeModal.onclick = function(){
-  if(modal.classList.contains('active')){
-    modal.classList.remove('active');
-    modal.classList.add('desactive');
-  }
-}
+// const closeModal = document.getElementById('#close-modal').click();
+
+// const modal= () => {
+//   alert('deu certo')
+// }
+
+// openModal = function(){
+//   if(modal.classList.contains('desactive')){
+//     modal.classList.remove('desactive');
+//     modal.classList.add('active');
+//   }
+// }
+
+// closeModal = function(){
+//   if(modal.classList.contains('active')){
+//     modal.classList.remove('active');
+//     modal.classList.add('desactive');
+//   }
+// }
+
+// function iniciaModal(){
+//   const modal = document.querySelector('.box-modal-details');
+//   // if(modal.classList.contains('desactive')){
+//   //   modal.classList.remove('desactive');
+//   //   modal.classList.add('active');
+//   // }
+
+//   alert('deu certo');
+// }
+
+// const openModal = document.getElementById('#modal_toggle');
+
+// openModal.addEventListener('click', () => {
+//   iniciaModal();
+// })
+
+// const modal = document.querySelector('.box-modal-details');
+
+// const openModal = document.getElementById('box-content-result-2');
+// openModal.addEventListener('click', () => {
+//   alert('deu certo')
+// })
+
 
 const initialValueHeroComuted = `&nameStartsWith=spider`;
 
-let BASE_URL = `${BASE_URL_INITIAL}limit=20&ts=${TIME}&apikey=${API_KEY_PUBLIC}&hash=${API_PRIVATE_HASH}`
-// fetchApi(BASE_URL);
+let BASE_URL = `${BASE_URL_INITIAL}limit=10&ts=${TIME}&apikey=${API_KEY_PUBLIC}&hash=${API_PRIVATE_HASH}`
 
 
-const form = document.querySelector("form");
-form.addEventListener("submit", event => {
-  event.preventDefault();
-  console.log(event)
-  const {value} = document.querySelector('#hero');
-  let valueHeroComuted = '';
-  if(value === ''){
-    valueHeroComuted = '';
-  }else{
-    valueHeroComuted = `&nameStartsWith=${value}`;
+let PAGE_RESULTS = 1;
+
+
+
+
+const heroisContainer = document.querySelector('#list-hero');
+
+const getHerois = async (heroi = '', pagina) => {
+  if(heroi !== ''){
+    BASE_URL = `${BASE_URL}&name=${heroi}`
+  }
+
+  const response = await fetch(BASE_URL);
+
+  return response.json();
+}
+
+const addHeroisIntoDom = async (heroi, page) => {
+  const data = await getHerois(heroi, page);
+
+  
+  
+  
+  const totalRecordsPerPage = 10;
+  const totalRecords = data.data.total;
+  const totalPages = Math.round(totalRecords / totalRecordsPerPage);
+  let currentPage = page === 1 ? 1 : page;
+  
+  if (currentPage > totalPages) {
+    currentPage = totalPages;
+  } else if (currentPage < 1) {
+    currentPage = 1;
   }
   
-  const BASE_URL = `${BASE_URL_INITIAL}limit=10&ts=${TIME}&apikey=${API_KEY_PUBLIC}&hash=${API_PRIVATE_HASH}${valueHeroComuted}`;
-  fetchApi(BASE_URL);
-});
+  const begin = (totalRecordsPerPage * currentPage) - totalRecordsPerPage;
 
+  // faz uma nova requisição para api para população dos dados com paginação
+  let heroisNovaChamada = fetch(`${BASE_URL}&offset=${begin}`).then(response => response.json()).then(response => response.data)
+  heroisNovaChamada = await heroisNovaChamada; 
+  
 
+  
+  
+  const herois = heroisNovaChamada.results;
 
-function buscarHeroi(){
-  console.log('estou aqui')  
-};
+  const detailSerie = document.querySelector('.box_detail_series');
 
-const buscando = (value) => {
-  return value;
-};
+  const heroesTemplate = herois.map((heroi) => 
+  {
+    const srcImage =  heroi.thumbnail.path +'.'+heroi.thumbnail.extension;
+    const template = `
+      <li>
+        <div class="box_detail_hero">
+          <div class="box_thumb">
+          <img src="${srcImage}" alt="${heroi.name}" />
+          </div>
+          <p>${heroi.name}</p>
+        </div>
+        <div class="box_detail_series">
+          <p>Series</>
+        </div>
+        <div class="box_detail_events">
+          <p>ajadlahlahd</>
+        </div>
+      </li>
+    `;
 
+    return template;
+  }  
+ ).join('');
 
-function fetchApi(base){
-  const marvel = fetch(`${base}`)
-                        .then(response => response.json())
-                        .then(({data}) => {
-                          
-                          
-                          data.results.forEach((result) => {
-                            // const divHero = document.querySelector('#hero'); 
-                            
-                            let liHero = document.createElement('li');
+heroisContainer.innerHTML += heroesTemplate;
 
-                            liHero.classList.add('hero-class');
-                            const srcImage =  result.thumbnail.path +'.'+result.thumbnail.extension;
-                            createDivHero(srcImage, result, liHero);                
-                          })
-                        })
-
-  return marvel;
 }
 
 
+const divPaginação = document.querySelector('.box-pagination ul li a');
+
+console.log(divPaginação.dataset)
 
 
+addHeroisIntoDom('', PAGE_RESULTS);
 
-const father = document.querySelector('#list-hero');
-
-
-
-
-function createDivHero(image, hero, divHero){
-  const contentHtml = `
-            <div class="box_detail_hero">
-              <div class="box_thumb">
-                <img src="${image}" alt="${hero.name}" />
-              </div>
-              <p>${hero.name}</p>
-            </div>
-            <div class="box_detail_series">
-              <p>serie</p>
-              <p>serie</p>
-              <p>serie</p>
-            </div>
-            <div class="box_detail_events">
-              <p>Events 1</p>
-              <p>Events 1</p>
-              <p>Events 1</p>
-            </div>  
-  `;
-
-  divHero.innerHTML = contentHtml;
-
-  father.appendChild(divHero);
-
-
-} 
-
-// fetchApi(BASE_URL);
+const form = document.querySelector('#form-herois');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputValue = document.querySelector('#hero');
+  const value = inputValue.value;
+  addHeroisIntoDom(value, 5);
+})
